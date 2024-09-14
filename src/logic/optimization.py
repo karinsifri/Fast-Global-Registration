cd165ddd233539dc79b9d9c6b4b971e8e4386232
Karin Sifri <karin.sifri@gmail.com>
2024-09-14 13:30:39 +0300

    add docstrings for all functions

--- src/logic/optimization.py
+++ src/logic/optimization.py
@@ -8,20 +8,22 @@ from src.utils.transforms import apply_homogenous_transformation, project_to_rig
 
 def optimization_step(p: np.ndarray, q: np.ndarray, t: np.ndarray, mu: float) -> np.ndarray:
     """
-    TODO: write docstring
+    Updates the transformation matrix according to the optimization step
 
     Args:
-        p:
-        q:
-        t:
-        mu:
+        p (np.ndarray): the matching points from the first point cloud
+        q (np.ndarray): the matching points from the second point cloud
+        t (np.ndarray): the current value of the transformation matching the first point cloud to the second point cloud
+        mu (float): the Geman-McClure scale (as described in section 3.1)
 
     Returns:
-
+        t (np.ndarray): the updated transformation matrix calculated after the optimization step
     """
+    # calculate the l weights according to the partial derivatives (formula 6)
     dists: np.ndarray = np.linalg.norm(p - apply_homogenous_transformation(points=q, transformation_matrix=t), axis=1)
     l_weights: np.ndarray = (mu / (mu + dists ** 2)) ** 2
 
+    # find the new transformation using Levenberg-Marquardt algorithm
     xi_0 = 0, 0, 0, 0, 0, 0
     result = least_squares(pairwise_objective, xi_0, args=(p, q, t, l_weights), method='lm')
 
--- src/utils/linearization.py
+++ src/utils/linearization.py
@@ -3,35 +3,18 @@ import numpy as np
 
 def reconstruct_transformation(t: np.ndarray, xi: tuple[float, float, float, float, float, float]) -> np.ndarray:
     """
-    TODO: write docstring
+    Given a linearization of a transformation matrix from its previous value, reconstruct the new transformation matrix
 
     Args:
-        t:
-        xi:
+        t (np.ndarray): the previous transformation matrix
+        xi (tuple): the linearizarion of the new transformation matrix from the previous transformation matrix
 
     Returns:
-
+        t_new (np.ndarray): the constructed transformation matrix
     """
-    return xi_to_mat(*xi) @ t
-
-
-def xi_to_mat(alpha: float, beta: float, gamma: float, a: float, b: float, c: float) -> np.ndarray:
-    """
-    TODO: write docstring
-
-    Args:
-        alpha:
-        beta:
-        gamma:
-        a:
-        b:
-        c:
-
-    Returns:
-
-    """
+    alpha, beta, gamma, a, b, c = xi
     mat = np.array([[1, -gamma, beta, a],
                     [gamma, 1, -alpha, b],
                     [-beta, alpha, 1, c],
                     [0, 0, 0, 1]])
-    return mat
+    return mat @ t
