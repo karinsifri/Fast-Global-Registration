ff0b5cf3673a375f371d80d0d27463bab6434a5b
Karin Sifri <karin.sifri@gmail.com>
2024-09-21 14:32:44 +0300

    add point normalization

--- src/logic/registration.py
+++ src/logic/registration.py
@@ -10,9 +10,10 @@ MAX_ITERATIONS = 64  # TODO: move to consts
 def pairwise_registration(pcd_p: PointCloud, pcd_q: PointCloud) -> np.ndarray:
     # TODO: write docstring
     points_p, points_q = find_point_correspondence(pcd_p, pcd_q)
+    points_p, points_q = normalize_points(points_p, points_q)
 
     t = np.identity(4)
-    mu = 100  # TODO: set to be the max diameter of the point clouds
+    mu = 1
 
     for i in range(MAX_ITERATIONS):
         t = optimization_step(points_p, points_q, t, mu)
@@ -21,3 +22,12 @@ def pairwise_registration(pcd_p: PointCloud, pcd_q: PointCloud) -> np.ndarray:
             mu /= 2
 
     return t
+
+
+def normalize_points(points_p: np.ndarray, points_q: np.ndarray) -> tuple[np.ndarray, np.ndarray]:
+    mean_p = np.mean(points_p, axis=0)
+    mean_q = np.mean(points_q, axis=0)
+    scale = np.linalg.norm(points_p - mean_p, axis=1).max()
+    new_p = (points_p - mean_p) / scale
+    new_q = (points_q - mean_q) / scale
+    return new_p, new_q
