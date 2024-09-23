7e60f7d87d232e16131bcb2004899b3a204f3c4d
Karin Sifri <karin.sifri@gmail.com>
2024-09-23 08:44:11 +0300

    fix visualization for the demo

--- demo/utils/visualizers.py
+++ demo/utils/visualizers.py
@@ -3,28 +3,28 @@ from matplotlib import pyplot as plt
 import matplotlib.colors as mcolors
 
 
-def visualize_surfaces(surface1, surface2, points1, points2, l):
+def visualize_step_2d(spline1: np.ndarray, spline2: np.ndarray, points1: np.ndarray, points2: np.ndarray, l_weights: np.ndarray, step: int) -> None:
     # Create a figure and axis
     fig, ax = plt.subplots(figsize=(10, 8))
 
     # Plot the surfaces
-    ax.plot(surface1[:, 0], surface1[:, 1], label='Surface 1', color='blue')
-    ax.plot(surface2[:, 0], surface2[:, 1], label='Surface 2', color='red')
+    ax.plot(spline1[:, 0], spline1[:, 1], label='Spline 1', color='blue')
+    ax.plot(spline2[:, 0], spline2[:, 1], label='Spline 2', color='red')
 
     # Scatter the points on both surfaces
-    ax.scatter(points1[:, 0], points1[:, 1], color='blue', s=50, label='Points on Surface 1', edgecolor='black')
-    ax.scatter(points2[:, 0], points2[:, 1], color='red', s=50, label='Points on Surface 2', edgecolor='black')
+    ax.scatter(points1[:, 0], points1[:, 1], color='blue', s=50, label='Points on Spline 1', edgecolor='black')
+    ax.scatter(points2[:, 0], points2[:, 1], color='red', s=50, label='Points on Spline 2', edgecolor='black')
 
     # Normalize the l values to [0, 1] for the colormap
-    norm = mcolors.Normalize(vmin=np.min(l), vmax=np.max(l))
+    norm = mcolors.Normalize(vmin=np.min(l_weights), vmax=np.max(l_weights))
 
     # Create a colormap (using RdYlGn to represent good/bad scale)
-    cmap = plt.cm.RdYlGn
+    cmap = plt.cm.Greens
 
     # Plot the connections with colors representing the l values
     for i in range(len(points1)):
         ax.plot([points1[i, 0], points2[i, 0]], [points1[i, 1], points2[i, 1]],
-                color=cmap(norm(l[i])), alpha=0.6)
+                color=cmap(norm(l_weights[i])), alpha=0.6)
 
     # Add a colorbar to show the scale of l values
     sm = plt.cm.ScalarMappable(cmap=cmap, norm=norm)
@@ -35,10 +35,13 @@ def visualize_surfaces(surface1, surface2, points1, points2, l):
     # Set labels and title
     ax.set_xlabel('X')
     ax.set_ylabel('Y')
-    ax.set_title('Surface Visualization with Points and Connections')
+    ax.set_title(f'Step {step}')
 
     # Add a legend
     ax.legend()
+
+    # Set aspect ratio
+    ax.set_aspect("equal")
 
     # Show the plot
     plt.show()
